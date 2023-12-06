db7e2336b97a237dbb2bfaca8c5f92212a3835bf
kunalsingh1301 <kunal.singh@iitg.ac.in>
2023-12-06 15:28:58 +0530

    Partners done& data changes required

--- src/pages/partners/Partners.js
+++ src/pages/partners/Partners.js
@@ -1,15 +1,23 @@
-import styles from './partners.module.css'
-import NavBar from '../../components/navbar/Navbar'
+import styles from './partners.module.css';
+import Partnering from './partnering';
+import Upper2 from './Upper2';
+import Wypar from './Wypar';
+import OurAudience from './OurAudience';
+import SponsOptunity from './SponsOptunity';
 
 const Partners = () => {
-    return(
-        <>
-        <NavBar></NavBar>
-        <h1 className = {styles.main}>
-            PARTNERS PAGE
-        </h1>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.main}>
+        {/* <NavBar></NavBar> */}
+        <Partnering />
+        <Upper2 />
+        <Wypar />
+        <OurAudience />
+        <SponsOptunity />
+      </div>
+    </>
+  );
 }
 
-export default Partners
+export default Partners;
