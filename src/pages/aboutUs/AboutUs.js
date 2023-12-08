206413a73d900ac4802b6fdfb5224c5e7c01ff00
Durgesh Kumar Deo <durgeshdeo1410@gmail.com>
2023-12-08 23:28:40 +0530

    Made About Us page

--- src/pages/aboutUs/AboutUs.js
+++ src/pages/aboutUs/AboutUs.js
@@ -1,15 +1,192 @@
-import styles from './aboutUs.module.css'
-import NavBar from '../../components/navbar/Navbar'
+import styles from "./aboutUs.module.css";
+import NavBar from "../../components/navbar/Navbar";
 
 const AboutUs = () => {
-    return(
-        <>
-        <NavBar></NavBar>
-        <h1 className = {styles.main}>
-            ABOUT US PAGE
-        </h1>
-        </>
-    )
-}
+  return (
+    <>
+      <NavBar></NavBar>
 
-export default AboutUs
+      <div className={styles.outerbox}>
+        <div className={styles.innerbox1}>
+          <div className={styles.contentbox}>
+            <div className={styles.heading}>TED</div>
+            <div className={styles.content}>
+              <p>
+                Technology,Entertainmentand Design,the setof global conferences
+                have been going with the spirit of ideas worth spreading
+                since1984. TED isa global community, welcoming people from every
+                discipline and culture who seek a deeper understanding of the
+                world. TED believes passionately in the power of ideas to change
+                attitudes, lives and, ultimately, the world. TED is owned by a
+                nonprofit, nonpartisan foundation. The agenda is to make great
+                ideas accessible and spark conversation TheTEDx Program is
+                designed to help communities, organizations and individuals to
+                spark conversation TheTEDx Program is designed to help
+                communities, organizations and individuals to spark conversation
+                and connection through local TED-like experiences.
+              </p>
+              <p>
+                The TEDx Program is designed to help communities, organizations
+                and individuals to spark conversation and connection through
+                local TED-like experiences. At TEDx events, a screening of TED
+                Talks videos— ora combination of live presenters and TED Talks
+                videos— sparks deep conversation and connections at the local
+                level.TEDx events are planned and coordinated independently,
+                under a free license granted by TED
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.innerbox2}>
+          <div className={styles.contentbox2}>
+            <div className={styles.heading2}>
+              TED
+              <div>
+                <sup>x</sup>
+              </div>
+            </div>
+            <div className={styles.content2}>
+              <p>
+                In the spirit of ideas worth spreading, TEDx is a program of
+                local, self-organized events that bring people to get her to
+                share aTED-like experience. Ata TEDx event, TED Talks video and
+                live speakers combine to spark deep discussion and connection in
+                a small group. These local, self-organized events are branded
+                TEDx, where x = independently organized TED event. The TED
+                Conference provides general guidance for the TEDx program, but
+                individual TEDx events are self-organized.(Subject to certain
+                rules and regulations.)
+              </p>
+            </div>
+          </div>
+          <div className={styles.contentbox3}>
+            <iframe
+              className={styles.vdoframe}
+              width="710"
+              height="415"
+              src="https://www.youtube.com/embed/d0NHOpeczUU?si=u2AxAGd0nERdJ1E4"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <div className={styles.mainContent}>
+        <div className={styles.teamHeader}>
+          <div className={styles.teamHeading}>
+            <span>Meet our leadership team</span>
+          </div>
+          <div className={styles.teamHeader2}>
+            <p>
+              The leadership team here at TEDxLondon is made up of a diverse
+              bunch of awesome humans!
+            </p>
+          </div>
+          <div className={styles.teamHeaderContent}>
+            <p>
+              We are driven by passion and curiosity. We are inspired by ideas.
+              And we want to leave the world better than we found it. In our day
+              jobs you can find us working in design, conservation, innovation,
+              marketing, finance, NGOs, arts, business, public policy, health
+              and everything in between. We work year round to bring TEDxLondon
+              and all of our activities to life:
+            </p>
+          </div>
+        </div>
+        <div>
+          <div className={styles.teamImg}>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.teamImg}>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.teamImg}>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+            <div>
+              <div className={styles.imgBox}>
+                <img src="/Image.jpeg" alt="Img" />
+              </div>
+              <div className={styles.imgTitle}>
+                <div className={styles.imgTitleName}>XYZ</div>
+                <div className={styles.imgTitlePost}>Secretary</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AboutUs;
