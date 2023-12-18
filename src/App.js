2dd2624057ea28b1ca280323bbc72f329bdadd23
Shaurya <shauryajain202@gmail.com>
2023-12-18 22:19:54 +0530

    added event23

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/home/Home'
 import Events from './pages/events/eventsIndex'
+import Event_23 from "./pages/events/2023/Event_23.js"
+import Event_22 from "./pages/events/2023/Event_22.js"
+import Event_21 from "./pages/events/2023/Event_21.js"
 import AboutUs from './pages/aboutUs/AboutUs'
 import ContactUs from './pages/contactUs/ContactUs'
 import Partners from './pages/partners/Partners'
@@ -17,6 +20,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Events" element={<Events/>} />
+        <Route path="/Events/2023" element={<Event_23/>} />
+        <Route path="/Events/2022" element={<Event_22/>} />
+        <Route path="/Events/2021" element={<Event_21/>} />
         <Route path="/AboutUs" element={<AboutUs/>} />
         <Route path="/Partners" element={<Partners />} />
         <Route path="/ContactUs" element={<ContactUs/>} />
