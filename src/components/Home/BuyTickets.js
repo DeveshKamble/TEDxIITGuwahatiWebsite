cd6f94d03b1f23a3e89165658bae47075a82a290
Ashish <kodudula3@gmail.com>
2023-12-27 20:27:44 +0530

    Changes to Home

--- src/components/Home/BuyTickets.js
+++ src/components/Home/BuyTickets.js
@@ -10,12 +10,12 @@ const BuyTickets = () => {
         <img src='/Images/partners/buytick.webp' alt='buy-Ticket' style={{ aspectRatio: '16/9', objectFit: 'cover' }} height={200} />
       </picture>
       <div style={{ marginBottom: '20px', background: 'red' }}>
-        <ul style={{ display: 'flex' }}>
+        {/* <ul style={{ display: 'flex' }}>
           <li>Let's Connect</li>
           <li>l</li>
           <li>i</li>
           <li>f</li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   );
