61013c631fc3e93b48b075f898199e2fc56b6e4d
kunalsingh1301 <kunal.singh@iitg.ac.in>
2023-12-28 10:49:25 +0530

    chnages

--- src/components/Home/BuyTickets.js
+++ src/components/Home/BuyTickets.js
@@ -10,12 +10,7 @@ const BuyTickets = () => {
         <img src='/Images/partners/buytick.webp' alt='buy-Ticket' style={{ aspectRatio: '16/9', objectFit: 'cover' }} height={200} />
       </picture>
       <div style={{ marginBottom: '20px', background: 'red' }}>
-        {/* <ul style={{ display: 'flex' }}>
-          <li>Let's Connect</li>
-          <li>l</li>
-          <li>i</li>
-          <li>f</li>
-        </ul> */}
+        
       </div>
     </div>
   );
