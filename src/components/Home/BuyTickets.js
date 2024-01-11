68f986fea926beaee6bad4251a16b5526f947d98
kunalsingh1301 <kunal.singh@iitg.ac.in>
2024-01-11 21:56:38 +0530

    ..

--- src/components/Home/BuyTickets.js
+++ src/components/Home/BuyTickets.js
@@ -20,19 +20,18 @@ const BuyTickets = () => {
         <div className={styles.innerbox}
         >
           <div className={styles.date}
-          >
+            >
             <h3>11 February</h3><br />
             <h3>2024</h3>
           </div>
           <hr className={styles.line} />
           <div className={styles.time}
-          >
+            >
             <h5 >9 AM to 5 PM</h5>
           </div>
           <hr className={styles.line} />
           <div className={styles.booking_tickets}>
             <p style={{ textAlign: 'center', color: 'white' }}
-
             >The room is open to all who are looking for exemplary stories and inspiring ideas – and to participate in celebrating these individuals who embody the spirit of TED.</p>
             <a href='/Registration' style={{ textAlign: 'center', color: 'white', textDecoration: 'none', margin: '10px' }}>
               <button style={{ textAlign: 'center', border: '2px solid white', borderRadius: '10px', padding: '10px', marginTop: '10px' }}>
