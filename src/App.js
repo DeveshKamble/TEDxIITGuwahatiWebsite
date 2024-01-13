60a6a620b4b5e8098b7feaa7275950f22c11cfe7
Ashish <kodudula3@gmail.com>
2024-01-13 20:16:56 +0530

    Minor stylings

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import Partners from './pages/partners/Partners'
 import Registration from './pages/registration/Registration'
 import Privacy from './pages/privacy/Privacy'
 import Terms from './pages/terms/Terms'
-// import PaymentRedirect from "./pages/paymentRedirect/Redirect.js"
-// import Success from "./pages/success/success.js"
+import PaymentRedirect from "./pages/paymentRedirect/Redirect.js"
+import Success from "./pages/success/success.js"
 
 function App() {
 
